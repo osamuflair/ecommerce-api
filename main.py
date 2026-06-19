7320f108eba6a3f03fedcd920bd0185dc389c791
osamuflair <osamudiamenbenedict05@gmail.com>
2026-06-19 11:37:02 +0100

    feat: create app entrypoint and wire up users and address routers

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from app.api.v1.routes.users import router as users_router
+from app.api.v1.routes.address import router as address_router
+
+
+app.include_router(users_router, prefix="/api/v1")
+app.include_router(address_router, prefix="/api/v1")
