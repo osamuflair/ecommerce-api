09c39a1b66e120ef8e6ff4ac14494a15bcc6e7aa
osamuflair <osamudiamenbenedict05@gmail.com>
2026-06-19 15:00:08 +0100

    fix: import all models in models/__init__.py to register SQLAlchemy relationships

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.address import Address
+from app.models.cart import Cart, CartItem
+from app.models.category import Category
+from app.models.order import Order, OrderItem, OrderStatus
+from app.models.product import Product
+from app.models.user import User, UserRole
+from app.models.wishlist import Wishlist, WishlistItem
