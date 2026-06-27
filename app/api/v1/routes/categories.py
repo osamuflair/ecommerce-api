432d779206ec352262c8a32135156182c91bb663
osamuflair <osamudiamenbenedict05@gmail.com>
2026-06-27 14:23:17 +0100

    feat: add category endpoints with public read and admin-gated write access

---
+++ app/api/v1/routes/categories.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends
+from app.services.category_service import create_category, update_category, delete_category, get_all_category, get_category_by_id
+from app.schemas.category import CategoryCreate, CategoryUpdate, CategoryResponse
+from typing import Annotated
+from sqlalchemy.orm import Session
+from app.core.database import get_session
+from app.core.security import get_current_user, require_admin
+from app.models.user import User
+
+router = APIRouter(
+    prefix = "/categories",
+    tags = ["categories"]
+)
+
+@router.post("/")
+def category_create(db: Annotated[Session, Depends(get_session)], admin: Annotated[User, Depends(require_admin)], created_category: CategoryCreate):
+    create_category(db, created_category)
+    return {"Message": "Category Successfully Created"}
+
+@router.put("/{category_id}")
+def category_update(db: Annotated[Session, Depends(get_session)], admin: Annotated[User, Depends(require_admin)], category_id: int, updated_category: CategoryUpdate):
+    update_category(db, category_id, updated_category)
+    return {"Message": "Category Successfully Updated"}
+
+@router.delete("/{category_id}")
+def category_delete(db: Annotated[Session, Depends(get_session)], admin: Annotated[User, Depends(require_admin)], category_id: int):
+    delete_category(db, category_id)
+    return {"Message": "Category Successfully Deleted"}
+
+@router.get("/", response_model = list[CategoryResponse])
+def categories_get_all(db: Annotated[Session, Depends(get_session)]):
+    return get_all_category(db)
+
+@router.get("/{category_id}", response_model = CategoryResponse)
+def category_get_by_id(db: Annotated[Session, Depends(get_session)], category_id: int):
+    return get_category_by_id(db, category_id)
