cdfe254bd5f5c4ef1e9f30c15ff2338131362be7
osamuflair <osamudiamenbenedict05@gmail.com>
2026-07-10 13:44:29 +0100

    docs: add docstrings to category_service

--- app/services/category_service.py
+++ app/services/category_service.py
@@ -5,6 +5,22 @@ from app.schemas.category import CategoryCreate, CategoryUpdate
 from fastapi import HTTPException
 
 def create_category(db: Session, created_category: CategoryCreate) -> Category:
+    """
+    Creates a category for products.
+
+    Validates that the category does not already exists.
+    Creates category.
+
+    Args:
+        db: DB session.
+        created_category: A CategoryCreate model that contains the name and description of the category.
+
+    Returns:
+        The created Category model.
+
+    Raises:
+        HTTPException 400: If the category already exists.
+    """
     existing_category = db.query(Category).filter(Category.name == created_category.name).first()
     if existing_category:
         raise HTTPException(status_code = 400, detail = "Category already exists")
@@ -18,6 +34,23 @@ def create_category(db: Session, created_category: CategoryCreate) -> Category:
     return new_category
 
 def update_category(db: Session, category_id: int, updated_category: CategoryUpdate) -> Category:
+    """
+    Updates the name or/and description of a category.
+    
+    Validates that the category exists.
+    Updates the category.
+
+    Args:
+        db: DB session.
+        category_id: ID of the category
+        updated_category: A CategoryUpdate model that contains the name or/and description of the category.
+
+    Returns:
+        The updated Category model.
+
+    Raises:
+        HTTPException 404: If the category does not exists.
+    """
     category = db.query(Category).filter(Category.id == category_id).first()
     if not category:
         raise HTTPException(status_code = 404, detail = "Category not found")
@@ -30,6 +63,24 @@ def update_category(db: Session, category_id: int, updated_category: CategoryUpd
     return category
 
 def delete_category(db: Session, category_id: int) -> Category:
+    """
+    Deletes a category.
+    
+    Validates that the category exists.
+    Validates that the category has no existing products.
+    Deletes the category.
+
+    Args:
+        db: DB session.
+        category_id: ID of the category.
+
+    Returns:
+        The deleted Category model.
+
+    Raises:
+        HTTPException 404: If the category does not exists.
+        HTTPException 409: If the category has existing products.
+    """
     category = db.query(Category).filter(Category.id == category_id).first()
     if not category:
         raise HTTPException(status_code = 404, detail = "Category not found")
@@ -41,10 +92,26 @@ def delete_category(db: Session, category_id: int) -> Category:
     return category
 
 def get_category_by_id(db: Session, category_id: int) -> Category:
+    """
+    Gets a category by its ID.
+    
+    Validates that the category exists.
+
+    Args:
+        db: DB session.
+        category_id: ID of the category
+
+    Returns:
+        The Category model.
+
+    Raises:
+        HTTPException 404: If the category does not exists.
+    """
     existing_category = db.query(Category).filter(Category.id == category_id).first()
     if not existing_category:
         raise HTTPException(status_code = 404, detail = "category not found")
     return existing_category
 
 def get_all_category(db: Session) -> list[Category]:
+    """Gets all the categories"""
     return db.query(Category).all()
